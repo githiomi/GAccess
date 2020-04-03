08bb9242cec551ea80901a881538e448c5310a83
Dhosio <danielgithiomi@gmail.com>
2020-04-03 14:14:40 +0300

    added the link to environment.ts and backed it in gitignore

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  danielGithubLink : "https://api.github.com/users/daneden?access_token=76be0661ccc938cb4b9fcb67a82bf05cce9c7a03",
   production: false
 };
 
